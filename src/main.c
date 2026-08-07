b37c38599ec0e2c1c4d5c7b74e8916cb3957b9a8
MoodyStarrr <lucas.borges@outlook.fr>
2026-08-07 14:02:05 +0200

    Used watchdog to simulate bug and reset Arduino after a certain time

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <avr/wdt.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <uart.h>
@@ -11,17 +12,6 @@
 void set_DDRB(int bit){
 	DDRB |= ( 1 << bit ); // DDRB = Data Direction Register pour le port B. Permet de configurer chaque pin du port B en entrée 0 ou sortie 1
 }
-/* Explications : Avec OU | on compare les bits de DDRB avec les bits de (1 << bit). 
- * L'opérateur OU | dit que si au moins un des 2 bits est à 1 alors le resultat est 1.
- * L'opérateur << (bit-shift) decale les bits vers la gauche (>> vers la droite). ( a << b ) permet donc 
- * de décaler un nombre a de b positions vers la gauche.
- * 
- * Exemple : (1 << 5) = 0 0 0 0 1 0 0 0
- *   0 0 0 0 0 0 0 0 | (1 << 5) 
- * = 0 0 0 0 0 0 0 0 | 0 0 0 0 1 0 0 0 = 0 0 0 0 1 0 0 0
- *
- * Le bit à la position 5 à donc bien été mis à 1 sans toucher le reste de l'octet.
- * */
 
 void config_PORTB(int state, int bit){
 	if( state  == 0 ){
@@ -30,22 +20,14 @@ void config_PORTB(int state, int bit){
 		PORTB |= (1 << bit);
 	}
 }
-/* Explcations : Avec ET & on compare les bits de PORTB avec les bits de (1 << bit)
- * L'opérateur ET & dit que les 2 bits doivent etre à 1 pour sortir 1
- *
- * Avec NOT ~ on inverse chaque bit
- *
- * Exemple: 
- * 1 0 1 0 & ~(0 0 1 0) = 1 0 1 0 & 1 1 0 1 = 1 0 0 0 
- *
- * Le bit à la position 3 à bien été mis à 0 sans toucher le reste de l'octet
- *
- * */
 
 volatile ring_buffer buffer;
 
+volatile uint8_t count_time = 0;
+
 ISR(TIMER1_COMPA_vect){
 	PORTB ^= (1 << PORTB5);
+  count_time += 1;
 }
 
 ISR(USART_RX_vect){
@@ -69,13 +51,7 @@ int main(void){
 	for(int i = 0; i < strlen(str) - 1; i++){
 		uart_putchar(str[i]);
 	}
-  /* TP3	
-	char received[4];
-	received[0] = uart_getchar();
-	received[1] = uart_getchar();
-	received[2] = uart_getchar();
-	received[3] = uart_getchar();
-  */
+
   for(int i = 0; i < strlen(str) - 1;i++){
     received[i] = ring_buffer_get(&buffer);
   }
@@ -83,6 +59,11 @@ int main(void){
 	for(int i = 0; i < strlen(str) - 1; i++){
 		uart_putchar(received[i]);
 	}
+
+  wdt_enable(WDTO_2S);
+
 	while(1){
+    if(count_time <= 10) //10 itérations de count_time correspondent à 5s
+      wdt_reset();
 	}
 }
